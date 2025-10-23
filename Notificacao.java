201627b3e9e8937254cee7fcf3ed3884dca6b980
Fckleber <134643500+Fckleber@users.noreply.github.com>
2025-10-22 23:03:08 -0300

    Delete Notificacao.java

--- Notificacao.java
+++
@@ -1,6 +0,0 @@
-package SistemaNotificacao;
-
-public interface Notificacao {
-	// Este é o método que será implementado de formas diferentes (Polimorfismo)
-	void enviar(String destinatario, String mensagem);
-}
